1a1a0c8515b9105bd9e08223c3712f6a6699d592
Chenyun Zhang <morningchenyun@gmail.com>
2020-11-16 16:26:20 -0500

    update

--- mod-4-project/share-frontend/src/Components/TodayInHistory.js
+++ mod-4-project/share-frontend/src/Components/TodayInHistory.js
@@ -7,9 +7,6 @@ function TodayInHistory() {
   const monthIndex = date.getMonth();
   const m = monthIndex + 1;
   const [quote, setQuote] = useState("");
-  // const hours = date.getHours()
-  // const minutes = date.getMinutes()
-  // const seconds = date.getSeconds()
 
   useEffect(() => {
     fetch(`http://numbersapi.com/${m}/${d}/date`)
@@ -19,23 +16,11 @@ function TodayInHistory() {
       });
   }, []);
 
-  // const getTodayHistory =() =>{
-  //   fetch(`http://numbersapi.com/${m}/${d}/date`)
-  //   .then((r) => r.text())
-  //   .then((quoteOftheday) => {
-  //     setQuote(quoteOftheday);
-  //   });
-  // }
-
-  // const handleClick =() =>{
-  //   getTodayHistory()
-  // }
 
   return (
     <div>
       <div className="item">
         <h2>Today in History</h2>
-        {/* <i class="redo alternate icon" onClick={handleClick}></i> */}
         <div className="ui segment news-item">
           <p>{quote}</p>
         </div>
